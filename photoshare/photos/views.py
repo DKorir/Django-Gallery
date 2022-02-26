0ca9671a6eca48e59953f2541bc8df55e0651cad
DKorir <dominic.korir@student.moringaschool.com>
2022-02-26 16:14:26 +0300

    create views for category in gallery class  model

--- photoshare/photos/views.py
+++ photoshare/photos/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Category, Photo
 
 # Create your views here.
 def gallery(request):
-    return render(request,'photos/gallery.html')
+    category = request.GET.get('category')
+    if category == None:
+        photos = Photo.objects.all()
+    else:
+        photos = Photo.objects.filter(category__name=category)
+    categories = Category.objects.all()
+    context = {'categories':categories}
+    context = { 'categories': categories, 'photos': photos }
+    return render(request,'photos/gallery.html', context)
 
 def viewPhoto(request, pk):
     return render(request,'photos/photo.html')
