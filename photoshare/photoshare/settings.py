d036271bfadba6f26ebd1f0c14a77f72f50511e5
DKorir <dominic.korir@student.moringaschool.com>
2022-02-26 15:42:36 +0300

    create database

--- photoshare/photoshare/settings.py
+++ photoshare/photoshare/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'photoshare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogallery',
+        'USER':'moringa',
+        'PASSWORD':'0723120546',
     }
 }
 
