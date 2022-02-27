51995b3ca867b4acbc2d8a62863c0cb53717aaee
DKorir <dominic.korir@student.moringaschool.com>
2022-02-27 22:37:44 +0300

    update location functionalities with delete,update and save

--- photoshare/photos/models.py
+++ photoshare/photos/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from sqlalchemy import delete
 
 # Create your models here.
 class Location(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
-
+    def save_location(self):
+        self.save
+    def delete_location(self):
+        self.delete()
+    def update_location(self,name):
+        self.name
+        self.save
     def __str__(self):
         return self.name
 class Category(models.Model):
