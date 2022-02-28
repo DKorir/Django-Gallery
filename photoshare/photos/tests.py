5f3df24bb873ba63f87ff4412dc474195ab153f6
DKorir <dominic.korir@student.moringaschool.com>
2022-02-28 18:23:49 +0300

    create location test instance

--- photoshare/photos/tests.py
+++ photoshare/photos/tests.py
@@ -1,4 +1,12 @@
+from re import L
 from django.test import TestCase
 
 # Create your tests here.
 from .models import Photo,Location,Category
+class LocationTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.kisumu= Location(name = 'kisumu',)
+    def test_instance(self):
+        self.assertTrue(isinstance(self.kisumu,Location))
